923d0558635c12d022d875f97248e03a785e9090
Xardas7 <dawid.pado@hotmail.com>
2022-02-09 15:13:14 +0100

    update

--- microservices/Localizator/initialize_db.py
+++ microservices/Localizator/initialize_db.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-con = sqlite3.connect('reservations.db')
+con = sqlite3.connect('database.db')
 
 try:
     with con:
-        con.execute('CREATE TABLE IPs (name text, ip text)') #where to send data
-        con.execute('CREATE TABLE position (museum text, position text, booking_id text)')
+        con.execute('CREATE TABLE IPs (id text, name text, ip text)') #where to send data
+        con.execute('CREATE TABLE position (id text, museum text, position text, booking_id text)')
         
 except sqlite3.Error:
     print("Error initializing database")
